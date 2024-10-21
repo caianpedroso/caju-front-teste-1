ddbdba823accf7f3bc1f38d6580b80b464395104
Caian Egidio <caianpedroso@gmail.com>
2024-10-21 17:49:49 -0300

    feat: add vercel plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import eslintPlugin from "@nabla/vite-plugin-eslint";
+import vercel from "vite-plugin-vercel"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslintPlugin()],
+  plugins: [react(), eslintPlugin(, vercel()],
   server: {
     port: 3001,
   },
